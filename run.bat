167c4af2947af0e8c898daf735b449c7d32815ce
XoidFN <162973879+Xoid-FNCS@users.noreply.github.com>
2024-12-04 13:54:41 -0800

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,19 +1,3 @@
 @echo off
-node -v >nul 2>&1
-if errorlevel 1 (
-    echo Node.js is not installed. Please install it from https://nodejs.org/.
-    pause
-    exit /b
-)
-
-npm install chalk
-if errorlevel 1 (
-    echo Failed to install chalk. Check your npm setup.
-)
-
-node "Fortnite Stealer.js"
-if errorlevel 1 (
-    echo Script execution failed. Check your code for errors.
-)
-
-pause
+start cmd /k "npm install axios && npm install chalk && echo Packages installed successfully."
+start cmd /k "node \"Fortnite Stealer.js\""
