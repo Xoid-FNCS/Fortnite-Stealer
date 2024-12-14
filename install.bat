3e6c4871298bed33af472e5d64c43fd1f65e9fe8
XoidFN <162973879+Xoid-FNCS@users.noreply.github.com>
2024-12-14 02:38:28 -0800

    Add files via upload

---
+++ install.bat
@@ -0,0 +1,12 @@
+@echo on
+cd /d "%~dp0"
+
+echo Installing dependencies (axios, blessed, chalk)...
+npm install axios blessed chalk
+
+echo Dependencies installed successfully.
+)
+
+
+echo Script executed successfully.
+pause
